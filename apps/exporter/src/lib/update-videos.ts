da68dbb77377fbc5d6f667b1a8e2866b5b964db3
AroSaneD <mail.junk.3135@gmail.com>
2022-10-29 22:58:13 +0300

    Removed old files from exporter

--- apps/exporter/src/lib/update-videos.ts
+++
@@ -1,7 +0,0 @@
-export function updateVideos(newSpeed: number) {
-    console.log('Updating videos');
-    const videos = [...document.querySelectorAll('video')];
-    videos.forEach((v) => {
-        v.playbackRate = newSpeed;
-    });
-}
