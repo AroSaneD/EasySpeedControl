ca2527455d1467e1dccd2445a526d60da010c5d8
AroSaneD <mail.junk.3135@gmail.com>
2023-05-14 19:24:31 +0300

    Removed ejs as a dependency

--- apps/exporter/src/build-script.ts
+++ apps/exporter/src/build-script.ts
@@ -1,4 +1,3 @@
-import * as ejs from 'ejs';
 import path from 'path';
 import fs from 'fs';
 import { DateTime } from 'luxon';
@@ -15,16 +14,15 @@ const fileToCreate = distDir + '/userscript.js';
 
 const jsBundle = fs.readFileSync(`${distDir}/speed.es.js`).toString();
 const cssBundle = fs.readFileSync(`${distDir}/style.css`).toString();
-const template = fs.readFileSync(`${currentDir}/src/template.js`).toString();
+const template = fs.readFileSync(`${currentDir}/src/template.txt`).toString();
 
 const currDate = DateTime.now().toISODate();
 
 fs.writeFileSync(
     fileToCreate,
     // jsBundle
-    ejs.render(template, {
-        jsBundle,
-        cssBundle,
-        currDate,
-    } as TemplateData)
+    template
+        .replace('$jsBundle', jsBundle)
+        .replace('$cssBundle', cssBundle)
+        .replace('$currentDate', currDate)
 );
